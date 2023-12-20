71bf974e1c3b891f37bcdd09a682c8e1128e1d50
uekann <kantaueno0808@yahoo.co.jp>
2023-12-20 09:55:12 +0900

    nodeにidを振る matchの修正

--- src/lib/NFA.java
+++ src/lib/NFA.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 sealed public class NFA permits DFA{
     private static class Node {
-        public HashMap<Character, HashSet<Node>> transitions;
+        HashMap<Character, HashSet<Node>> transitions;
+        int id;
         Node(){
             transitions = new HashMap<>();
+            id = 0;
         }
 
         public void addTransition(Character c, Node n){
@@ -26,13 +28,29 @@ sealed public class NFA permits DFA{
 
     Node start;
     Node end;
-    HashSet<Node> nodes;
+    HashMap<Integer ,Node> nodes;
     public NFA(){
         start = new Node();
+        start.id = 0;
         end = new Node();
-        nodes = new HashSet<>();
-        nodes.add(start);
-        nodes.add(end);
+        end.id = 1;
+        nodes = new HashMap<>();
+        nodes.put(0, start);
+        nodes.put(1, end);
+    }
+
+    private static HashMap<Integer, Node> _concatNodes(HashMap<Integer, Node>... nodesList){
+        HashMap<Integer, Node> nodes = new HashMap<>();
+        HashSet<Node> values = new HashSet<>();
+        for(HashMap<Integer, Node> nodesMap : nodesList){
+            values.addAll(nodesMap.values());
+        }
+        List<Node> valuesList = new ArrayList<>(values);
+        for(int i = 0; i < values.size(); i++){
+            nodes.put(i, valuesList.get(i));
+            valuesList.get(i).id = i;
+        }
+        return nodes;
     }
 
     public static NFA fromAST(AST ast){
@@ -50,21 +68,17 @@ sealed public class NFA permits DFA{
                 nfa.start.addTransition(null, nfa2.start);
                 nfa1.end.addTransition(null, nfa.end);
                 nfa2.end.addTransition(null, nfa.end);
-                nfa.nodes.addAll(nfa1.nodes);
-                nfa.nodes.addAll(nfa2.nodes);
+                nfa.nodes = _concatNodes(nfa.nodes, nfa1.nodes, nfa2.nodes);
                 yield nfa;
             }
             case AST.Concat<?, ?> t -> {
                 NFA nfa1 = fromAST(t.t1);
                 NFA nfa2 = fromAST(t.t2);
-                NFA nfa = new NFA();
-                nfa.nodes.remove(nfa.start);
-                nfa.start = nfa1.start;
                 nfa1.end.addTransition(null, nfa2.start);
-                nfa.nodes.remove(nfa.end);
+                NFA nfa = new NFA();
+                nfa.start = nfa1.start;
                 nfa.end = nfa2.end;
-                nfa.nodes.addAll(nfa1.nodes);
-                nfa.nodes.addAll(nfa2.nodes);
+                nfa.nodes = _concatNodes(nfa1.nodes, nfa2.nodes);
                 yield nfa;
             }
             case AST.Star<?> t -> {
@@ -74,35 +88,40 @@ sealed public class NFA permits DFA{
                 nfa.start.addTransition(null, nfa.end);
                 nfa1.end.addTransition(null, nfa1.start);
                 nfa1.end.addTransition(null, nfa.end);
-                nfa.nodes.addAll(nfa1.nodes);
+                nfa.nodes = _concatNodes(nfa.nodes, nfa1.nodes);
                 yield nfa;
             }
             case AST.Group<?> t -> fromAST(t.t);
         };
     }
 
+    private HashSet<Node> _epsilonClosure(HashSet<Node> nodes){
+        nodes = new HashSet<>(nodes);
+        HashSet<Node> epsilon = new HashSet<>(nodes);
+        for(int i = 0; i < this.nodes.size(); i++){
+            for(Node n : nodes){
+                if(n.transitions.containsKey(null)){
+                    epsilon.addAll(n.transitions.get(null));
+                }
+            }
+            nodes.addAll(epsilon);
+        }
+        return nodes;
+    }
+
     public boolean match(String s){
         HashSet<Node> current = new HashSet<>((List.of(start)));
         for(int i = 0; i < s.length(); i++){
+            current = _epsilonClosure(current);
             HashSet<Node> next = new HashSet<>();
             for(Node n : current){
                 if(n.transitions.containsKey(s.charAt(i))){
                     next.addAll(n.transitions.get(s.charAt(i)));
                 }
             }
-            // イプシロン展開
-            HashSet<Node> epsilon = new HashSet<>(next);
-            for(int j = 0; j < this.nodes.size(); j++){
-//                System.out.println(next.size());
-                for (Node n : next) {
-                    if (n.transitions.containsKey(null)) {
-                        epsilon.addAll(n.transitions.get(null));
-                    }
-                }
-                next.addAll(epsilon);
-            }
             current = next;
         }
+        current = _epsilonClosure(current);
         return current.contains(end);
     }
 }
